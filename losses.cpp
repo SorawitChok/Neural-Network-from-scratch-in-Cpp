1d1e2bed2a03c4670a9339389a3835d0fc15cade
SirawitC <sirawitchok@hotmail.com>
2024-08-14 15:28:31 +0700

    MSE loss

--- losses.cpp
+++ losses.cpp
@@ -1,13 +1,24 @@
 #include <vector>
 #include <math.h>
+#include<cmath>
 using namespace std;
 
-double binaryCrossEntropy(vector<int> true_label, vector<double> pred_prob){
+double BCELoss(vector<int> true_label, vector<double> pred_prob){
     double sum = 0;
     for(int i=0; i < pred_prob.size() ; i++){
         sum += true_label[i]*log10(pred_prob[i]) + (1-true_label[i])*log10((1-pred_prob[i]));
     }
     int size = true_label.size();
     double loss = -(1.0/size)*sum; 
+    return loss;
+}
+
+double MSELoss(vector<double> true_label, vector<double> pred){
+    double sum = 0;
+    for(int i=0; i < true_label.size() ; i++){
+        sum += pow(true_label[i] - pred[i],2.0);
+    }
+    int size = true_label.size();
+    double loss = (1.0/size)*sum;
     return loss;
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <vector>
 #include "activation.cpp"
+#include "losses.cpp"
 
 using namespace std;
 int main()
 {
-    return 0;
+    vector<double> true_lab = {3, -0.5, 2, 7};
+    vector<double> pred_lab = {2.5, 0.0, 2, 8};
+    double loss = MeanSquareError(true_lab, pred_lab);
+    printf("%lf",loss);
 }
