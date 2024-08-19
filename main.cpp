0de8f4473ab227d37e7bf8e8248c1f48d5960cb8
SirawitC <sirawitchok@hotmail.com>
2024-08-19 14:36:47 +0700

    merge main

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main()
 {
     vector<double> true_lab = {3, -0.5, 2, 7};
     vector<double> pred_lab = {2.5, 0.0, 2, 8};
-    double loss = MeanSquareError(true_lab, pred_lab);
+    double loss = MSELoss(true_lab, pred_lab);
     printf("%lf",loss);
 }
