2efe0a4e17b2d8abfd73c8b3ae10e66a32504233
sorawitC <sorawitchok@hotmail.com>
2024-08-20 10:50:06 +0700

    add transpose function

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <vector>
-#include "layer.cpp"
-#include "losses.cpp"
+// #include "layer.cpp"
+// #include "losses.cpp"
+#include "utils.cpp"
 
 int main()
 {
-    vector<double> true_lab = {3, -0.5, 2, 7};
-    vector<double> pred_lab = {2.5, 0.0, 2, 8};
-    double loss = MSELoss(true_lab, pred_lab);
-    printf("%lf", loss);
+    std::vector<std::vector<double>> test = {{1, 2}, {3, 4}, {5, 6}};
+    std::vector<std::vector<double>> x = transpose(test);
+
+    printf("%lf %lf\n", test[0][0], test[0][1]);
+    printf("%lf %lf\n", test[1][0], test[1][1]);
+    printf("%lf %lf\n", test[2][0], test[2][1]);
+    printf("======================\n");
+    printf("%lf %lf %lf\n", x[0][0], x[0][1], x[0][2]);
+    printf("%lf %lf %lf\n", x[1][0], x[1][1], x[1][2]);
 }
