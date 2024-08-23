f9d48f5b81918f4f646d16d51b46318e2d1658de
sorawitC <sorawitchok@hotmail.com>
2024-08-23 20:36:32 +0700

    update Layer override method

--- layer.cpp
+++ layer.cpp
@@ -7,18 +7,20 @@ class Layer
 public:
     std::vector<double> input;
     std::vector<double> output;
+    virtual std::vector<double> forward(const std::vector<double> &input_data) = 0;
+    virtual std::vector<double> backward(std::vector<double> error, double learning_rate) = 0;
 };
 
 class Sigmoid : public Layer
 {
 public:
-    std::vector<double> forward(const std::vector<double> &input_data)
+    std::vector<double> forward(const std::vector<double> &input_data) override
     {
         input = input_data;
         output = vectSigmoid(input);
         return output;
     }
-    std::vector<double> backward(std::vector<double> error, double learning_rate)
+    std::vector<double> backward(std::vector<double> error, double learning_rate) override
     {
         std::vector<double> derivative = vectSigmoidDerivative(input);
         std::vector<double> grad_input;
@@ -34,13 +36,13 @@ public:
 class Relu : public Layer
 {
 public:
-    std::vector<double> forward(const std::vector<double> &input_data)
+    std::vector<double> forward(const std::vector<double> &input_data) override
     {
         input = input_data;
         output = vectRelu(input);
         return output;
     }
-    std::vector<double> backward(std::vector<double> error, double learning_rate)
+    std::vector<double> backward(std::vector<double> error, double learning_rate) override
     {
         std::vector<double> derivative = vectReluDerivative(input);
         std::vector<double> grad_input;
@@ -57,13 +59,13 @@ class LeakyRelu : public Layer
 {
 public:
     double alpha = 0.01;
-    std::vector<double> forward(const std::vector<double> &input_data)
+    std::vector<double> forward(const std::vector<double> &input_data) override
     {
         input = input_data;
         output = vectLeakyRelu(input, alpha);
         return output;
     }
-    std::vector<double> backward(std::vector<double> error, double learning_rate)
+    std::vector<double> backward(std::vector<double> error, double learning_rate) override
     {
         std::vector<double> derivative = vectLeakyReluDerivative(input, alpha);
         std::vector<double> grad_input;
@@ -79,13 +81,13 @@ public:
 class Tanh : public Layer
 {
 public:
-    std::vector<double> forward(const std::vector<double> &input_data)
+    std::vector<double> forward(const std::vector<double> &input_data) override
     {
         input = input_data;
         output = vectTanh(input);
         return output;
     }
-    std::vector<double> backward(std::vector<double> error, double learning_rate)
+    std::vector<double> backward(std::vector<double> error, double learning_rate) override
     {
         std::vector<double> derivative = vectTanhDerivative(input);
         std::vector<double> grad_input;
@@ -114,7 +116,7 @@ public:
         bias = biasInitailizer(num_out);
     }
 
-    std::vector<double> forward(const std::vector<double> &input_data)
+    std::vector<double> forward(const std::vector<double> &input_data) override
     {
         input = input_data;
         for (int i = 0; i < output_neuron; i++)
@@ -124,33 +126,33 @@ public:
 
         return output;
     }
-    std::vector<double> backward(std::vector<double> error, double learning_rate)
+    std::vector<double> backward(std::vector<double> error, double learning_rate) override
     {
         std::vector<double> input_error;  // dE/dX
         std::vector<double> weight_error; // dE/dW
         std::vector<double> bias_error;   // dE/dB
 
         std::vector<std::vector<double>> weight_transpose = transpose(weights);
-        bias_error = error;
+        // bias_error = error;
 
-        for (int i = 0; i < weight_transpose.size(); i++)
-        {
-            input_error[i] = dotProduct(weight_transpose[i], error);
-        }
+        // for (int i = 0; i < weight_transpose.size(); i++)
+        // {
+        //     input_error[i] = dotProduct(weight_transpose[i], error);
+        // }
 
-        for (int j = 0; j < error.size(); j++)
-        {
-            for (int i = 0; i < input.size(); i++)
-            {
-                weight_error[j][i] = error[j] * input[i];
-            }
-        }
+        // for (int j = 0; j < error.size(); j++)
+        // {
+        //     for (int i = 0; i < input.size(); i++)
+        //     {
+        //         weight_error[j][i] = error[j] * input[i];
+        //     }
+        // }
 
-        bias = subtract(bias, learning_rate * bias_error);
-        for (int i; weight_error.size(); i++)
-        {
-            weights[i] = subtract(weights[i], learning_rate * weight_error[i]);
-        }
-        return input_error;
+        // bias = subtract(bias, learning_rate * bias_error);
+        // for (int i; weight_error.size(); i++)
+        // {
+        //     weights[i] = subtract(weights[i], learning_rate * weight_error[i]);
+        // }
+        // return input_error;
     }
 };
